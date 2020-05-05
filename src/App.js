fd1e96eca2de2abe7f51b92d6545a49c052c260b
Huynh Dieu Tuong <huynhdieutuong@gmail.com>
2020-05-05 15:36:35 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const todos = ['Go to market', 'Buy food', 'Make dinner'];
+
+    return (
+      <div className='App'>
+        <ul>
+          {todos.map((todo) => (
+            <li>{todo}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
